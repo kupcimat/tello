1783dd37636e6dde7e4eac4ca7c15c14704f4270
Matej Kupciha <matej.kupciha@gooddata.com>
2019-04-20 14:27:15 +0700

    Introduce tello command executor

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
-import asyncio
 import sys
 
-import tello.tello as tello
-import tello.tello_command as command
+from tello.tello import Tello
+from tello.tello_command import Command, GenericCommand
 
 if __name__ == "__main__":
-    tello_commands = [command.Command()] + [command.GenericCommand(x) for x in sys.argv[1:]]
-    asyncio.run(
-        tello.send_commands(tello_commands))
+    tello = Tello()
+    tello_commands = [Command()] + [GenericCommand(cmd) for cmd in sys.argv[1:]]
+
+    for tello_command in tello_commands:
+        tello.execute_command(tello_command)
+
+    tello.join()
--- tello/tello.py
+++ tello/tello.py
@@ -1,6 +1,66 @@
 import asyncio
+import threading
 
 from tello.tello_protocol import TelloProtocol
+
+
+class Tello:
+    normal_priority = 100
+    high_priority = 1
+
+    def __init__(self):
+        self.executor_loop = asyncio.new_event_loop()
+        self.command_queue = asyncio.PriorityQueue(loop=self.executor_loop)
+        self.command_count = 0
+        # Start tello commands executor thread
+        threading.Thread(target=start_executor,
+                         args=(self.executor_loop, self.command_queue),
+                         name="command-executor").start()
+
+    def execute_command(self, tello_command, priority=normal_priority):
+        print("Execute command:", tello_command.get_command())
+        self.command_count += 1
+        task = (priority, self.command_count, tello_command)
+        asyncio.run_coroutine_threadsafe(
+            self.command_queue.put(task), self.executor_loop).result()
+
+    def join(self):
+        asyncio.run_coroutine_threadsafe(
+            self.command_queue.join(), self.executor_loop).result()
+        self.close()
+
+    def close(self):
+        tasks = asyncio.all_tasks(self.executor_loop)
+        asyncio.run_coroutine_threadsafe(
+            cancel_executor_tasks(tasks), self.executor_loop).result()
+        self.executor_loop.call_soon_threadsafe(stop_executor, self.executor_loop)
+
+
+def start_executor(loop, command_queue):
+    try:
+        asyncio.set_event_loop(loop)
+        loop.create_task(command_executor(command_queue))
+        loop.run_forever()
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+
+
+def stop_executor(loop):
+    loop.stop()
+
+
+async def cancel_executor_tasks(tasks):
+    for task in tasks:
+        task.cancel()
+    await asyncio.gather(*tasks, return_exceptions=True)
+
+
+async def command_executor(command_queue):
+    while True:
+        _, _, tello_command = await command_queue.get()
+        await send_command(tello_command)
+        command_queue.task_done()
 
 
 async def send_command(tello_command):
@@ -17,16 +77,14 @@ async def send_command(tello_command):
         await asyncio.wait_for(done_callback, timeout=socket_timeout)
     except asyncio.TimeoutError:
         print("Network timeout, close the socket")
+    except asyncio.CancelledError:
+        print("Task cancelled, close the socket")
+        raise
     finally:
         transport.close()
         # workaround for asynchronous socket close
         # https://stackoverflow.com/questions/35872750/how-to-close-python-asyncio-transport
         await asyncio.sleep(0)
-
-
-async def send_commands(tello_commands):
-    for tello_command in tello_commands:
-        await send_command(tello_command)
 
 
 # Initialize module
