4bad2f1c006233e884a2ba6574625b8882c1c366
Matej Kupciha <kupciham@hotmail.com>
2020-05-25 15:22:57 +0200

    Add command receive thread for v2

--- client2.py
+++ client2.py
@@ -1,6 +1,8 @@
 import logging
 import socket
 
+import time
+
 import kupcimat.command as cmd
 import kupcimat.tello as tello
 
@@ -9,13 +11,18 @@ logging.basicConfig(level=logging.DEBUG)
 logging.info("Initialize sockets")
 cmd_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cmd_socket.bind(("0.0.0.0", 8889))
-
 video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 video_socket.bind(("0.0.0.0", 11111))
 
-logging.info("Initialize tello")
-logging.info(tello.send_cmd(cmd_socket, cmd.command()))
-logging.info(tello.send_cmd(cmd_socket, cmd.stream_on()))
+logging.info("Initialize threads")
+cmd_thread = tello.ReceiveCmdThread(cmd_socket)
+cmd_thread.start()
 
-logging.info(tello.send_cmd(cmd_socket, cmd.get_temperature()))
-logging.info(tello.send_cmd(cmd_socket, cmd.get_battery()))
+logging.info("Initialize tello")
+tello.send_cmd(cmd_socket, cmd.command())
+tello.send_cmd(cmd_socket, cmd.stream_on())
+
+tello.send_cmd(cmd_socket, cmd.get_temperature())
+tello.send_cmd(cmd_socket, cmd.get_battery())
+
+time.sleep(10)
--- kupcimat/tello.py
+++ kupcimat/tello.py
@@ -1,17 +1,31 @@
 import logging
+import threading
 from socket import socket
 from typing import Tuple
+
+
+def tello_timeout() -> int:
+    return 5
 
 
 def tello_address() -> Tuple[str, int]:
     return "192.168.10.1", 8889
 
 
-def send_cmd(cmd_socket: socket, command: str) -> str:
+def send_cmd(cmd_socket: socket, command: str) -> None:
     logging.info("action=send-command command=%s", command)
     cmd_socket.sendto(command.encode(), tello_address())
-    # TODO error handling and timeout
-    response = None
-    while response is None:
-        response, address = cmd_socket.recvfrom(2048)
-    return response.decode().strip()
+
+
+class ReceiveCmdThread(threading.Thread):
+    def __init__(self, cmd_socket: socket):
+        super().__init__(name="cmd-thread", daemon=True)
+        self.socket = cmd_socket
+        self.response = None
+
+    def run(self) -> None:
+        while True:
+            # TODO error handling
+            (response, address) = self.socket.recvfrom(2048)
+            self.response = response.decode().strip()
+            logging.info("action=receive-response response=%s", self.response)
